b2838585f9034aba99739eae7c605213aa35c0e7
YutaYanagisawa2502046 <tky2502046@stu.o-hara.ac.jp>
2026-07-14 14:24:55 +0900

    Windows対応/ビルド

--- TapGame/Assets/Scripts/CameraShake.cs
+++ TapGame/Assets/Scripts/CameraShake.cs
@@ -43,7 +43,7 @@ public class CameraShake : MonoBehaviour
     /// カメラシェイクを開始します。
     /// </summary>
     /// <param name="duration">シェイクの持続時間（秒）</param>
-    /// <param name="amount">シェイクの振幅 ※大きくすると揺れが激しくなるため注意</param>
+    /// <param name="amount">シェイクの振幅 ※大きくすると揺れが激しくなって酔うため注意</param>
     public static void Shake(float duration, float amount)
     {
         // シングルトンインスタンスが存在しない場合、メインカメラにCameraShakeコンポーネントを追加
