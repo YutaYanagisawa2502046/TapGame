c8d279fabd86ac44c71e906d15c037cd112e456e
YutaYanagisawa2502046 <tky2502046@stu.o-hara.ac.jp>
2026-07-14 11:25:58 +0900

    SE変更 クリック/タップ時のエフェクト修正

--- TapGame/Assets/Scripts/ClickEffect.cs
+++ TapGame/Assets/Scripts/ClickEffect.cs
@@ -7,7 +7,7 @@ using System.Collections;
 public class ClickEffect : MonoBehaviour
 {
     public GameObject effectPrefab; // エフェクトのプレハブ
-    public float maxScaleMultiplier = 1.5f; // エフェクトが広がる大きさの倍率
+    public float maxScale = 1.5f; // エフェクトが広がる大きさ
     public float duration = 0.3f; // エフェクトの表示時間
 
     private Camera mainCamera;
@@ -22,6 +22,10 @@ public class ClickEffect : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            var color = effectPrefab.GetComponent<SpriteRenderer>().color;
+            color.a = 1f;
+            effectPrefab.GetComponent<SpriteRenderer>().color = color;
+
             // 画面をタップしたときの処理
             Vector2 tapPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
@@ -53,14 +57,22 @@ public class ClickEffect : MonoBehaviour
             }
 
             // ローカルスケールを変更
-            float scale = Mathf.Lerp(effectPrefab.transform.localScale.x, effectPrefab.transform.localScale.x * maxScaleMultiplier, elapsedTime / duration);
+            float scale = Mathf.Lerp(0f, maxScale, elapsedTime / duration);
+            Color effectColor = effectTransform.GetComponent<SpriteRenderer>().color;
+            effectColor.a = 1f - (scale / maxScale);
             effectTransform.localScale = new Vector2(scale, scale);
+            effectTransform.GetComponent<SpriteRenderer>().color = effectColor;
 
             // 経過時間を進める
             elapsedTime += Time.deltaTime;
 
             // 1フレーム進める
             yield return null;
+
         }
+
+        var color = effectPrefab.GetComponent<SpriteRenderer>().color;
+        color.a = 1f;
+        effectPrefab.GetComponent<SpriteRenderer>().color = color;
     }
 }
